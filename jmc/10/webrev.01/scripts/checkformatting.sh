ca449dc0740e8a7bdb0c4549c6b11564f3c39f96
duke <duke@openjdk.java.net>
2019-11-28 23:55:31 +0000

    Added webrev for jmc/10

---
+++ jmc/10/webrev.01/scripts/checkformatting.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -l
+
+set -e
+echo "======== Building p2 repo ==================="
+cd releng/third-party
+sh -c "mvn p2:site"
+sh -c "nohup mvn jetty:run &"
+cd ../../core
+echo "======== Installing core ======================"
+sh -c "mvn install"
+echo "======== Running spotless for core =========="
+sh -c "mvn spotless:check"
+echo "======== Running spotless for application ==="
+cd ..
+sh -c "mvn -Puitests spotless:check"
+echo "======== Finished ==========================="
+
