55ade471877c1a305289954684ce51455995bb02
duke <duke@openjdk.java.net>
2019-12-18 07:14:12 +0000

    Added webrev for skara/339

---
+++ skara/339/webrev.00-01/bots/notify/src/main/java/org/openjdk/skara/bots/notify/IssueUpdater.java
@@ -0,0 +1,164 @@
+/*
+ * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.openjdk.skara.bots.notify;
+
+import org.openjdk.skara.forge.*;
+import org.openjdk.skara.issuetracker.Issue;
+import org.openjdk.skara.issuetracker.*;
+import org.openjdk.skara.jcheck.JCheckConfiguration;
+import org.openjdk.skara.vcs.*;
+import org.openjdk.skara.vcs.openjdk.*;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.logging.Logger;
+
+public class IssueUpdater implements RepositoryUpdateConsumer, PullRequestUpdateConsumer {
+    private final IssueProject issueProject;
+    private final boolean reviewLink;
+    private final URI reviewIcon;
+    private final boolean commitLink;
+    private final URI commitIcon;
+    private final boolean setFixVersion;
+    private final String fixVersion;
+    private final Logger log = Logger.getLogger("org.openjdk.skara.bots.notify");
+
+    IssueUpdater(IssueProject issueProject, boolean reviewLink, URI reviewIcon, boolean commitLink, URI commitIcon,boolean setFixVersion, String fixVersion) {
+        this.issueProject = issueProject;
+        this.reviewLink = reviewLink;
+        this.reviewIcon = reviewIcon;
+        this.commitLink = commitLink;
+        this.commitIcon = commitIcon;
+        this.setFixVersion = setFixVersion;
+        this.fixVersion = fixVersion;
+    }
+
+    @Override
+    public void handleCommits(HostedRepository repository, Repository localRepository, List<Commit> commits, Branch branch) {
+        for (var commit : commits) {
+            var commitNotification = CommitFormatters.toTextBrief(repository, commit);
+            var commitMessage = CommitMessageParsers.v1.parse(commit);
+            for (var commitIssue : commitMessage.issues()) {
+                var optionalIssue = issueProject.issue(commitIssue.id());
+                if (optionalIssue.isEmpty()) {
+                    log.severe("Cannot update issue " + commitIssue.id() + " with commit " + commit.hash().abbreviate()
+                                       + " - issue not found in issue project");
+                    continue;
+                }
+                var issue = optionalIssue.get();
+                var existingComments = issue.comments();
+                var hashUrl = repository.webUrl(commit.hash()).toString();
+                var alreadyPostedComment = existingComments.stream()
+                                                           .filter(comment -> comment.author().equals(issueProject.issueTracker().currentUser()))
+                                                           .anyMatch(comment -> comment.body().contains(hashUrl));
+                if (!alreadyPostedComment) {
+                    issue.addComment(commitNotification);
+                }
+                issue.setState(Issue.State.RESOLVED);
+
+                if (commitLink) {
+                    var linkBuilder = Link.create(repository.webUrl(commit.hash()), "Commit")
+                                          .summary(repository.name() + "/" + commit.hash().abbreviate());
+                    if (commitIcon != null) {
+                        linkBuilder.iconTitle("Commit");
+                        linkBuilder.iconUrl(commitIcon);
+                    }
+                    issue.addLink(linkBuilder.build());
+                }
+
+                if (setFixVersion) {
+                    var requestedVersion = fixVersion;
+                    if (requestedVersion == null) {
+                        try {
+                            var conf = localRepository.lines(Path.of(".jcheck/conf"), commit.hash());
+                            if (conf.isPresent()) {
+                                var parsed = JCheckConfiguration.parse(conf.get());
+                                var version = parsed.general().version();
+                                requestedVersion = version.orElse(null);
+                            }
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+
+                    if (requestedVersion != null) {
+                        issue.addFixVersion(requestedVersion);
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void handleOpenJDKTagCommits(HostedRepository repository, Repository localRepository, List<Commit> commits, OpenJDKTag tag, Tag.Annotated annotated) {
+
+    }
+
+    @Override
+    public void handleTagCommit(HostedRepository repository, Repository localRepository, Commit commit, Tag tag, Tag.Annotated annotation) {
+
+    }
+
+    @Override
+    public void handleNewBranch(HostedRepository repository, Repository localRepository, List<Commit> commits, Branch parent, Branch branch) {
+
+    }
+
+    @Override
+    public boolean idempotent() {
+        return true;
+    }
+
+    @Override
+    public void handleNewIssue(PullRequest pr, org.openjdk.skara.vcs.openjdk.Issue issue) {
+        var realIssue = issueProject.issue(issue.id());
+        if (realIssue.isEmpty()) {
+            log.warning("Pull request " + pr + " added unknown issue: " + issue.id());
+            return;
+        }
+
+        if (reviewLink) {
+            var linkBuilder = Link.create(pr.webUrl(), "Review")
+                                  .summary(pr.repository().name() + "/" + pr.id());
+            if (reviewIcon != null) {
+                linkBuilder.iconTitle("Review");
+                linkBuilder.iconUrl(reviewIcon);
+            }
+
+            realIssue.get().addLink(linkBuilder.build());
+        }
+    }
+
+    @Override
+    public void handleRemovedIssue(PullRequest pr, org.openjdk.skara.vcs.openjdk.Issue issue) {
+        var realIssue = issueProject.issue(issue.id());
+        if (realIssue.isEmpty()) {
+            log.warning("Pull request " + pr + " removed unknown issue: " + issue.id());
+            return;
+        }
+
+        realIssue.get().removeLink(pr.webUrl());
+    }
+}
