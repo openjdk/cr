05da65b62d2c05bc0841890160a898fe8c818b1c
Robin Westberg <robin.westberg@oracle.com>
2020-04-14 12:31:35 +0000

    Replace old webrevs with redirects to improve publishing times

--- panama-foreign/69/webrev.01/src/jdk.incubator.jextract/share/classes/module-info.java
+++
@@ -1,9 +0,0 @@
-module jdk.incubator.jextract {
-    requires transitive java.compiler;
-    requires transitive jdk.incubator.foreign;
-    requires jdk.internal.opt;
-    exports jdk.incubator.jextract;
-
-    provides java.util.spi.ToolProvider with
-        jdk.incubator.jextract.tool.Main.JextractToolProvider;
-}
