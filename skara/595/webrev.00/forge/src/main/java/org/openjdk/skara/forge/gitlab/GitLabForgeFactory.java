b40af9c5595a3b0b52cbebcbbafa91170a4e0d60
duke <duke@openjdk.java.net>
2020-04-23 08:39:01 +0000

    Added webrev for skara/595

---
+++ skara/595/webrev.00/forge/src/main/java/org/openjdk/skara/forge/gitlab/GitLabForgeFactory.java
@@ -0,0 +1,33 @@
+package org.openjdk.skara.forge.gitlab;
+
+import org.openjdk.skara.forge.*;
+import org.openjdk.skara.host.Credential;
+import org.openjdk.skara.json.JSONObject;
+
+import java.net.URI;
+import java.util.Set;
+
+public class GitLabForgeFactory implements ForgeFactory {
+    @Override
+    public String name() {
+        return "gitlab";
+    }
+
+    @Override
+    public Set<String> knownHosts() {
+        return Set.of("gitlab.com");
+    }
+
+    @Override
+    public Forge create(URI uri, Credential credential, JSONObject configuration) {
+        var name = "GitLab";
+        if (configuration != null && configuration.contains("name")) {
+            name = configuration.get("name").asString();
+        }
+        if (credential != null) {
+            return new GitLabHost(name, uri, credential);
+        } else {
+            return new GitLabHost(name, uri);
+        }
+    }
+}
