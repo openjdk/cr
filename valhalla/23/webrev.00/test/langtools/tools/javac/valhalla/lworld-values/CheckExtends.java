49c8662d658cf52bf90012bcd0624f4ec4d3173a
Robin Westberg <robin.westberg@oracle.com>
2020-04-24 04:32:15 +0000

    Replace old webrevs with redirects to improve publishing times

--- valhalla/23/webrev.00/test/langtools/tools/javac/valhalla/lworld-values/CheckExtends.java
+++
@@ -1,9 +0,0 @@
-/*
- * @test /nodynamiccopyright/
- * @summary Values may not extend
- *
- * @compile/fail/ref=CheckExtends.out -XDallowEmptyValues -XDrawDiagnostics CheckExtends.java
- */
-
-final inline class CheckExtends extends Object {
-}
