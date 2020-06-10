de15ff71e6f19578a7cfd51f89626b4df0082b6a
duke <duke@openjdk.java.net>
2020-06-10 15:25:22 +0000

    Added webrev for skara/653

---
+++ skara/653/webrev.00/bots/notify/src/main/java/org/openjdk/skara/bots/notify/PullRequestWorkItem.java
@@ -0,0 +1,230 @@
+/*
+ * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.openjdk.skara.bots.notify;
+
+import org.openjdk.skara.bot.WorkItem;
+import org.openjdk.skara.forge.PullRequest;
+import org.openjdk.skara.json.*;
+import org.openjdk.skara.storage.StorageBuilder;
+import org.openjdk.skara.vcs.Hash;
+import org.openjdk.skara.vcs.openjdk.Issue;
+
+import java.nio.file.Path;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.regex.Pattern;
+import java.util.stream.*;
+
+public class PullRequestWorkItem implements WorkItem {
+    private final PullRequest pr;
+    private final StorageBuilder<PullRequestState> prStateStorageBuilder;
+    private final List<PullRequestListener> listeners;
+    private final Consumer<RuntimeException> errorHandler;
+    private final String integratorId;
+
+    PullRequestWorkItem(PullRequest pr, StorageBuilder<PullRequestState> prStateStorageBuilder, List<PullRequestListener> listeners, Consumer<RuntimeException> errorHandler, String integratorId) {
+        this.pr = pr;
+        this.prStateStorageBuilder = prStateStorageBuilder;
+        this.listeners = listeners;
+        this.errorHandler = errorHandler;
+        this.integratorId = integratorId;
+    }
+
+    private Hash resultingCommitHashFor(PullRequest pr) {
+       if (pr.labels().contains("integrated")) {
+           for (var comment : pr.comments()) {
+               if (comment.author().id().equals(integratorId)) {
+                   for (var line : comment.body().split("\n")) {
+                       if (line.startsWith("Pushed as commit")) {
+                           var parts = line.split(" ");
+                           var hash = parts[parts.length - 1].replace(".", "");
+                           return new Hash(hash);
+                       }
+                   }
+               }
+           }
+       }
+       return null;
+    }
+
+    private Set<PullRequestState> deserializePrState(String current) {
+        if (current.isBlank()) {
+            return Set.of();
+        }
+        var data = JSON.parse(current);
+        return data.stream()
+                   .map(JSONValue::asObject)
+                   .map(obj -> {
+                       var id = obj.get("pr").asString();
+                       var issues = obj.get("issues").stream()
+                                                     .map(JSONValue::asString)
+                                                     .collect(Collectors.toSet());
+
+                       // Storage might be missing commit information
+                       if (!obj.contains("commit")) {
+                           var prId = id.split(":")[1];
+                           var currentPR = pr.repository().pullRequest(prId);
+                           var hash = resultingCommitHashFor(currentPR);
+                           if (hash == null) {
+                               obj.putNull("commit");
+                           } else {
+                               obj.put("commit", hash.hex());
+                           }
+                       }
+
+                       var commit = obj.get("commit").isNull() ?
+                           null : new Hash(obj.get("commit").asString());
+
+                       return new PullRequestState(id, issues, commit);
+                   })
+                   .collect(Collectors.toSet());
+    }
+
+    private String serializePrState(Collection<PullRequestState> added, Set<PullRequestState> existing) {
+        var addedPrs = added.stream()
+                            .map(PullRequestState::prId)
+                            .collect(Collectors.toSet());
+        var nonReplaced = existing.stream()
+                                  .filter(item -> !addedPrs.contains(item.prId()))
+                                  .collect(Collectors.toSet());
+
+        var entries = Stream.concat(nonReplaced.stream(),
+                                    added.stream())
+                            .sorted(Comparator.comparing(PullRequestState::prId))
+                            .map(pr -> {
+                                var issues = new JSONArray(pr.issueIds()
+                                                             .stream()
+                                                             .map(JSON::of)
+                                                             .collect(Collectors.toList()));
+                                var commit = pr.commitId().isPresent()?
+                                    JSON.of(pr.commitId().get().hex()) : JSON.of();
+                                return JSON.object().put("pr", pr.prId())
+                                                    .put("issues",issues)
+                                                    .put("commit", commit);
+                            })
+                            .map(JSONObject::toString)
+                            .collect(Collectors.toList());
+        return "[\n" + String.join(",\n", entries) + "\n]";
+    }
+
+    private final Pattern issuesBlockPattern = Pattern.compile("\\n\\n###? Issues?((?:\\n(?: \\* )?\\[.*)+)", Pattern.MULTILINE);
+    private final Pattern issuePattern = Pattern.compile("^(?: \\* )?\\[(\\S+)]\\(.*\\): .*$", Pattern.MULTILINE);
+
+    private Set<String> parseIssues() {
+        var issuesBlockMatcher = issuesBlockPattern.matcher(pr.body());
+        if (!issuesBlockMatcher.find()) {
+            return Set.of();
+        }
+        var issueMatcher = issuePattern.matcher(issuesBlockMatcher.group(1));
+        return issueMatcher.results()
+                           .map(mo -> mo.group(1))
+                           .collect(Collectors.toSet());
+    }
+
+    @Override
+    public boolean concurrentWith(WorkItem other) {
+        if (!(other instanceof PullRequestWorkItem)) {
+            return true;
+        }
+        PullRequestWorkItem otherItem = (PullRequestWorkItem)other;
+        if (!pr.id().equals(otherItem.pr.id())) {
+            return true;
+        }
+        if (!pr.repository().name().equals(otherItem.pr.repository().name())) {
+            return true;
+        }
+        return false;
+    }
+
+    private void notifyNewIssue(String issueId) {
+        listeners.forEach(c -> c.onNewIssue(pr, new Issue(issueId, "")));
+    }
+
+    private void notifyRemovedIssue(String issueId) {
+        listeners.forEach(c -> c.onRemovedIssue(pr, new Issue(issueId, "")));
+    }
+
+    private void notifyNewPr(PullRequest pr) {
+        listeners.forEach(c -> c.onNewPullRequest(pr));
+    }
+
+    private void notifyIntegratedPr(PullRequest pr, Hash hash) {
+        listeners.forEach(c -> c.onIntegratedPullRequest(pr, hash));
+    }
+
+    @Override
+    public Collection<WorkItem> run(Path scratchPath) {
+        var historyPath = scratchPath.resolve("notify").resolve("history");
+        var storage = prStateStorageBuilder
+                .serializer(this::serializePrState)
+                .deserializer(this::deserializePrState)
+                .materialize(historyPath);
+
+        var issues = parseIssues();
+        var commit = resultingCommitHashFor(pr);
+        var state = new PullRequestState(pr, issues, commit);
+        var stored = storage.current();
+        if (stored.contains(state)) {
+            // Already up to date
+            return List.of();
+        }
+
+        // Search for an existing
+        var storedState = stored.stream()
+                .filter(ss -> ss.prId().equals(state.prId()))
+                .findAny();
+        if (storedState.isPresent()) {
+            var storedIssues = storedState.get().issueIds();
+            storedIssues.stream()
+                        .filter(issue -> !issues.contains(issue))
+                        .forEach(this::notifyRemovedIssue);
+            issues.stream()
+                  .filter(issue -> !storedIssues.contains(issue))
+                  .forEach(this::notifyNewIssue);
+
+            var storedCommit = storedState.get().commitId();
+            if (!storedCommit.isPresent() && state.commitId().isPresent()) {
+                notifyIntegratedPr(pr, state.commitId().get());
+            }
+        } else {
+            notifyNewPr(pr);
+            issues.forEach(this::notifyNewIssue);
+            if (state.commitId().isPresent()) {
+                notifyIntegratedPr(pr, state.commitId().get());
+            }
+        }
+
+        storage.put(state);
+        return List.of();
+    }
+
+    @Override
+    public String toString() {
+        return "Notify.PR@" + pr.repository().name() + "#" + pr.id();
+    }
+
+    @Override
+    public void handleRuntimeException(RuntimeException e) {
+        errorHandler.accept(e);
+    }
+}
---
+++ skara/653/webrev.00/bots/notify/src/main/java/org/openjdk/skara/bots/notify/RepositoryWorkItem.java
@@ -0,0 +1,303 @@
+/*
+ * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.openjdk.skara.bots.notify;
+
+import org.openjdk.skara.bot.WorkItem;
+import org.openjdk.skara.forge.*;
+import org.openjdk.skara.storage.StorageBuilder;
+import org.openjdk.skara.vcs.*;
+import org.openjdk.skara.vcs.openjdk.OpenJDKTag;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
+import java.util.stream.*;
+
+public class RepositoryWorkItem implements WorkItem {
+    private final Logger log = Logger.getLogger("org.openjdk.skara.bots");;
+    private final HostedRepository repository;
+    private final Path storagePath;
+    private final Pattern branches;
+    private final StorageBuilder<UpdatedTag> tagStorageBuilder;
+    private final StorageBuilder<UpdatedBranch> branchStorageBuilder;
+    private final List<RepositoryListener> listeners;
+
+    RepositoryWorkItem(HostedRepository repository, Path storagePath, Pattern branches, StorageBuilder<UpdatedTag> tagStorageBuilder, StorageBuilder<UpdatedBranch> branchStorageBuilder, List<RepositoryListener> listeners) {
+        this.repository = repository;
+        this.storagePath = storagePath;
+        this.branches = branches;
+        this.tagStorageBuilder = tagStorageBuilder;
+        this.branchStorageBuilder = branchStorageBuilder;
+        this.listeners = listeners;
+    }
+
+    private void handleNewRef(Repository localRepo, Reference ref, Collection<Reference> allRefs, RepositoryListener listener) throws NonRetriableException {
+        // Figure out the best parent ref
+        var candidates = new HashSet<>(allRefs);
+        candidates.remove(ref);
+        if (candidates.size() == 0) {
+            log.warning("No parent candidates found for branch '" + ref.name() + "' - ignoring");
+            return;
+        }
+
+        var bestParent = candidates.stream()
+                                   .map(c -> {
+                                       try {
+                                           return new AbstractMap.SimpleEntry<>(c, localRepo.commitMetadata(c.hash().hex() + ".." + ref.hash()));
+                                       } catch (IOException e) {
+                                           throw new UncheckedIOException(e);
+                                       }
+                                   })
+                                   .min(Comparator.comparingInt(entry -> entry.getValue().size()))
+                                   .orElseThrow();
+        if (bestParent.getValue().size() > 1000) {
+            throw new RuntimeException("Excessive amount of unique commits on new branch " + ref.name() +
+                                               " detected (" + bestParent.getValue().size() + ") - skipping notifications");
+        }
+        List<Commit> bestParentCommits;
+        try {
+            bestParentCommits = localRepo.commits(bestParent.getKey().hash().hex() + ".." + ref.hash(), true).asList();
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+        var branch = new Branch(ref.name());
+        var parent = new Branch(bestParent.getKey().name());
+        listener.onNewBranch(repository, localRepo, bestParentCommits, parent, branch);
+    }
+
+    private void handleUpdatedRef(Repository localRepo, Reference ref, List<Commit> commits, RepositoryListener listener) throws NonRetriableException {
+        var branch = new Branch(ref.name());
+        listener.onNewCommits(repository, localRepo, commits, branch);
+    }
+
+    private List<Throwable> handleRef(Repository localRepo, UpdateHistory history, Reference ref, Collection<Reference> allRefs) throws IOException {
+        var errors = new ArrayList<Throwable>();
+        var branch = new Branch(ref.name());
+        for (var listener : listeners) {
+            var lastHash = history.branchHash(branch, listener.name());
+            if (lastHash.isEmpty()) {
+                log.warning("No previous history found for branch '" + branch + "' and listener '" + listener.name() + " - resetting mark");
+                history.setBranchHash(branch, listener.name(), ref.hash());
+                try {
+                    handleNewRef(localRepo, ref, allRefs, listener);
+                } catch (NonRetriableException e) {
+                    errors.add(e.cause());
+                } catch (RuntimeException e) {
+                    // FIXME: Attempt rollback?
+                    errors.add(e);
+                }
+            } else {
+                var commitMetadata = localRepo.commitMetadata(lastHash.get() + ".." + ref.hash());
+                if (commitMetadata.size() == 0) {
+                    continue;
+                }
+                if (commitMetadata.size() > 1000) {
+                    history.setBranchHash(branch, listener.name(), ref.hash());
+                    errors.add(new RuntimeException("Excessive amount of new commits on branch " + branch.name() +
+                                                       " detected (" + commitMetadata.size() + ") for listener '" +
+                                                       listener.name() + "' - skipping notifications"));
+                    continue;
+                }
+
+                var commits = localRepo.commits(lastHash.get() + ".." + ref.hash(), true).asList();
+                history.setBranchHash(branch, listener.name(), ref.hash());
+                try {
+                    handleUpdatedRef(localRepo, ref, commits, listener);
+                } catch (NonRetriableException e) {
+                    errors.add(e.cause());
+                } catch (RuntimeException e) {
+                    // Attempt to roll back
+                    history.setBranchHash(branch, listener.name(), lastHash.get());
+                    errors.add(e);
+                }
+            }
+        }
+        return errors;
+    }
+
+    private Optional<OpenJDKTag> existingPrevious(OpenJDKTag tag, Set<OpenJDKTag> allJdkTags) {
+        while (true) {
+            var candidate = tag.previous();
+            if (candidate.isEmpty()) {
+                return Optional.empty();
+            }
+            tag = candidate.get();
+            if (!allJdkTags.contains(tag)) {
+                continue;
+            }
+            return Optional.of(tag);
+        }
+    }
+
+    private List<Throwable> handleTags(Repository localRepo, UpdateHistory history, RepositoryListener listener) throws IOException {
+        var errors = new ArrayList<Throwable>();
+        var tags = localRepo.tags();
+        var newTags = tags.stream()
+                          .filter(tag -> !history.hasTag(tag, listener.name()))
+                          .collect(Collectors.toList());
+
+        if (tags.size() == newTags.size()) {
+            if (tags.size() > 0) {
+                log.warning("No previous tag history found - ignoring all current tags");
+                history.addTags(tags, listener.name());
+            }
+            return errors;
+        }
+
+        if (newTags.size() > 10) {
+            history.addTags(newTags, listener.name());
+            errors.add(new RuntimeException("Excessive amount of new tags detected (" + newTags.size() +
+                                               ") - skipping notifications"));
+            return errors;
+        }
+
+        var allJdkTags = tags.stream()
+                             .map(OpenJDKTag::create)
+                             .filter(Optional::isPresent)
+                             .map(Optional::get)
+                             .collect(Collectors.toSet());
+        var newJdkTags = newTags.stream()
+                                .map(OpenJDKTag::create)
+                                .filter(Optional::isPresent)
+                                .map(Optional::get)
+                                .sorted(Comparator.comparingInt(OpenJDKTag::buildNum))
+                                .collect(Collectors.toList());
+        for (var tag : newJdkTags) {
+            var commits = new ArrayList<Commit>();
+
+            // Try to determine which commits are new since the last build
+            var previous = existingPrevious(tag, allJdkTags);
+            if (previous.isPresent()) {
+                commits.addAll(localRepo.commits(previous.get().tag() + ".." + tag.tag()).asList());
+            }
+
+            // If none are found, just include the commit that was tagged
+            if (commits.isEmpty()) {
+                var commit = localRepo.lookup(tag.tag());
+                if (commit.isEmpty()) {
+                    throw new RuntimeException("Failed to lookup tag '" + tag.toString() + "'");
+                } else {
+                    commits.add(commit.get());
+                }
+            }
+
+            Collections.reverse(commits);
+            var annotation = localRepo.annotate(tag.tag());
+
+            history.addTags(List.of(tag.tag()), listener.name());
+            try {
+                listener.onNewOpenJDKTagCommits(repository, localRepo, commits, tag, annotation.orElse(null));
+            } catch (NonRetriableException e) {
+                errors.add(e.cause());
+            } catch (RuntimeException e) {
+                errors.add(e);
+                history.retryTagUpdate(tag.tag(), listener.name());
+            }
+        }
+
+        var newNonJdkTags = newTags.stream()
+                                   .filter(tag -> OpenJDKTag.create(tag).isEmpty())
+                                   .collect(Collectors.toList());
+        for (var tag : newNonJdkTags) {
+            var commit = localRepo.lookup(tag);
+            if (commit.isEmpty()) {
+                throw new RuntimeException("Failed to lookup tag '" + tag.toString() + "'");
+            }
+
+            var annotation = localRepo.annotate(tag);
+
+            history.addTags(List.of(tag), listener.name());
+            try {
+                listener.onNewTagCommit(repository, localRepo, commit.get(), tag, annotation.orElse(null));
+            } catch (NonRetriableException e) {
+                errors.add(e.cause());
+            } catch (RuntimeException e) {
+                errors.add(e);
+                history.retryTagUpdate(tag, listener.name());
+            }
+        }
+
+        return errors;
+    }
+
+    @Override
+    public boolean concurrentWith(WorkItem other) {
+        if (!(other instanceof RepositoryWorkItem)) {
+            return true;
+        }
+        RepositoryWorkItem otherItem = (RepositoryWorkItem) other;
+        if (!repository.name().equals(otherItem.repository.name())) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Collection<WorkItem> run(Path scratchPath) {
+        var historyPath = scratchPath.resolve("notify").resolve("history");
+        var repositoryPool = new HostedRepositoryPool(storagePath.resolve("seeds"));
+
+        try {
+            var localRepo = repositoryPool.materialize(repository, scratchPath.resolve("notify").resolve("repowi").resolve(repository.name()));
+            var knownRefs = localRepo.remoteBranches(repository.url().toString())
+                                     .stream()
+                                     .filter(ref -> branches.matcher(ref.name()).matches())
+                                     .collect(Collectors.toList());
+            localRepo.fetchAll(false);
+
+            var history = UpdateHistory.create(tagStorageBuilder, historyPath.resolve("tags"), branchStorageBuilder, historyPath.resolve("branches"));
+            var errors = new ArrayList<Throwable>();
+
+            for (var listener : listeners) {
+                errors.addAll(handleTags(localRepo, history, listener));
+            }
+
+            boolean hasBranchHistory = !history.isEmpty();
+            for (var ref : knownRefs) {
+                if (!hasBranchHistory) {
+                    log.warning("No previous history found for any branch - resetting mark for '" + ref.name());
+                    for (var listener : listeners) {
+                        log.info("Resetting mark for branch '" + ref.name() + "' for listener '" + listener.name() + "'");
+                        history.setBranchHash(new Branch(ref.name()), listener.name(), ref.hash());
+                    }
+                } else {
+                    errors.addAll(handleRef(localRepo, history, ref, knownRefs));
+                }
+            }
+            if (!errors.isEmpty()) {
+                errors.forEach(error -> log.throwing("RepositoryWorkItem", "run", error));
+                throw new RuntimeException("Errors detected when processing repository notifications", errors.get(0));
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+        return List.of();
+    }
+
+    @Override
+    public String toString() {
+        return "RepositoryWorkItem@" + repository.name();
+    }
+}
